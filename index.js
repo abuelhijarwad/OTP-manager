2ddffc0284820ef75758381d754218be3ca5588d
abuelhijarwad <abuelhijarwad@gmail.com>
2024-06-25 03:44:22 +0300

    test

--- index.js
+++ index.js
@@ -1,39 +1,11 @@
-const crypto = require('crypto');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-class otp {
-    constructor() {
-      this.seed = Math.random();
-    }
-  
-     stringToNumber(str) {
-        let result = '';
-        for (let i = 0; i < str.length; i++) {
-          result += str.charCodeAt(i).toString();
-        }
-        return parseInt(result, 10);
-      }
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
 
-     creat(str, timestampToCheck) {
-        const timestamp = timestampToCheck || Math.floor(Date.now() / 1000);  // Current time in seconds
-        const combined = `${timestamp}${this.stringToNumber(str)}`;
-        const hash = crypto.createHash('sha256').update(combined).digest('hex');
-        const sixDigitCode = parseInt(hash.substring(0, 6), 16) % 1000000;
-        return sixDigitCode.toString().padStart(6, '0');
-    }
-
-     check(providedCode, username, mins = 5) {
-    const currentTimestamp = Math.floor(Date.now() / 1000);  // Current time in seconds
-    for (let i = 0; i <= 60*mins; i += 60) {  
-        const timestampToCheck = currentTimestamp - i;
-        const expectedCode = this.generate6DigitCode(username, timestampToCheck);
-        if (providedCode === expectedCode) {
-            return true;
-        }
-    }
-    return false;
-}
-
-
-}
-
-module.exports = otp;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
