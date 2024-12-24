aa073cd232f455e298a9f1178ba321c258e21790
abuelhijarwad <abuelhijarwad@gmail.com>
2024-12-24 11:41:10 +0200

    1.0.7

--- index.js
+++ index.js
@@ -14,24 +14,27 @@ class otp {
       }
 
      create(username, timestampToCheck) {
-        const timestamp = timestampToCheck || Math.floor(Date.now() / 1000);  // Current time in seconds
+        const timestamp = timestampToCheck || Math.floor(Date.now() / 60000);  // Current time in seconds
         const combined = `${timestamp}${this.stringToNumber(username)+this.seed}`;
         const hash = crypto.createHash('sha256').update(combined).digest('hex');
         const sixDigitCode = parseInt(hash.substring(0, 6), 16) % 1000000;
         return sixDigitCode.toString().padStart(6, '0');
     }
 
-     check(providedCode, username, mins = 5) {
-    const currentTimestamp = Math.floor(Date.now() / 1000);  // Current time in seconds
-    for (let i = 0; i <= 60*mins; i += 60) {  
-        const timestampToCheck = currentTimestamp - i;
-        const expectedCode = this.create(username, timestampToCheck);
-        if (providedCode === expectedCode) {
-            return true;
-        }
-    }
-    return false;
-    }
+    check(providedCode, username, mins = 5) {
+      const currentTimestamp = Math.floor(Date.now() / 60000); 
+      
+      for (let i = 0; i <= mins; i++) {  
+          const timestampToCheck = currentTimestamp - i;
+          const expectedCode = this.create(username, timestampToCheck);
+          
+          if (providedCode === expectedCode) {
+              return true;
+          }
+      }
+  
+      return false;
+  }
     reSeed() {
       this.seed = Math.floor(Math.random() * (64783892764 - 234234 + 1)) + 234234;
     }
